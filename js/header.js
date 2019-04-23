fb53102958ada3e8e459be247d6264bc867da7f8
namchee <cristophernamchee12@gmail.com>
2019-04-23 11:03:48 +0700

    Add box and modal

--- js/header.js
+++ js/header.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+  let navbar = document.querySelector('.navbar');
   let date = document.querySelector('#date');
   date.textContent = new Date().toLocaleString();
 
   setInterval(() => {
     date.textContent = new Date().toLocaleString();
   }, 500);
+
+  let prevScrollPos = window.pageYOffset;
+
+  window.onscroll = () => {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+      navbar.style.top = '0';
+    } else {
+      navbar.style.top = '-10vh';
+    }
+
+    prevScrollPos = currentScrollPos;
+  }
 });
--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   let modalButtons = document.querySelectorAll('button.modal-trigger');
-  console.log(modalButtons);
 
   for (let modalButton of modalButtons) {
     modalButton.addEventListener('click', () => {
       let modal = document.querySelector(`#${modalButton.dataset.target}`);
 
+      modal.classList.remove('remove');
       modal.classList.add('active');
       let overlay = modal.querySelector('.modal-overlay');
+      let closeButton = modal.querySelector('.modal-close-button');
 
       overlay.addEventListener('click', () => {
+        modal.classList.remove('active');
+        modal.classList.add('remove');
+      });
+
+      closeButton.addEventListener('click', () => {
         modal.classList.remove('active');
         modal.classList.add('remove');
       });
