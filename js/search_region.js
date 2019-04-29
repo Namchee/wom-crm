6c131e1ec2762fb2a06cc6bf1fe44a50efde1791
CRISTOPHER <i17017@ftis.unpar>
2019-04-29 09:11:33 +0700

    @.@

--- js/search_region.js
+++ js/search_region.js
@@ -1,5 +1,9 @@
 let client = new DataTable("#client");
 
+client.on('datatable.sort', () => {
+  toggleModals(); // g tau kenapa, cmn dia newasin
+})
+
 for (let row of client.activeRows) {
   row.addEventListener('click', () => {
     
