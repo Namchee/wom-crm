8fe066a42f9698826057367be7f6b471e888a512
HardjaTales77 <48974578+HardjaTales77@users.noreply.github.com>
2019-04-30 15:01:36 +0700

    Update clientModel.php

--- model/clientModel.php
+++ model/clientModel.php
@@ -3,20 +3,20 @@
 	class clientModel{
 			
 		public function getClient($namaClients){
-			$query = "SELECT * FROM client WHERE namaClient = $namaClients";
+			$query = "SELECT * FROM client WHERE namaClient = '$namaClients'";
 			$result = $db->executeSelectQuery($query);
 			return $result;
 		}
 		
 		public function setClient($namaClients,$nilai,$status,$alamat){
-			$query = "SELECT * FROM client WHERE namaClient = $namaClients";
+			$query = "SELECT * FROM client WHERE namaClient = '$namaClients'";
 			$result = $db->executeSelectQuery($query);
 			$nilaiBaru = $nilai;
 			$statusBaru = $status;
 			$alamatBaru = $alamat;
 			$nama = $namaClients;
 			if(isset($nama,$alamatBaru,$statusBaru,$nilaiBaru)){
-				$que = "UPDATE client SET nilaiInvestasi='$nilaiBaru',alamat='$alamatBaru',statusKawin='$statusBaru' WHERE namaClient='$nama'";
+				$que = "UPDATE client SET nilaiInvestasi=$nilaiBaru,alamat='$alamatBaru',statusKawin='$statusBaru' WHERE namaClient='$nama'";
 				$db->executeSelectQuery($que);
 			}
 		}
