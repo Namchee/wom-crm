360c59f8c52fc2090cadd0202acb5a5b8b3d819d
CRISTOPHER <i17017@ftis.unpar>
2019-05-02 13:46:57 +0700

    ...

--- model/clientModel.php
+++ model/clientModel.php
@@ -1,5 +1,27 @@
 <?php
     class Client {
-        
+        private $id;
+        private $nama;
+        private $statusKawin;
+        private $tanggalLahir;
+        private $nilaiInvest;
+        private $kota;
+        private $gender;
+        private $gambar;
+        private $cs;
+        private $age;
+
+        public function _construct ($id, $nama, $statusKawin, $tanggalLahir, $nilaiInvest, $kota, $gender, $gambar, $cs, $age) {
+            $this->id = $id;
+            $this->nama = $nama;
+            $this->statusKawin = $statusKawin;
+            $this->tanggalLahir = $tanggalLahir;
+            $this->nilaiInvest = $nilaiInvest;
+            $this->kota = $kota;
+            $this->gender = $gender;
+            $this->gambar = $gambar;
+            $this->cs = $cs;
+            $this->age = $age;
+        }
     }
 ?>
