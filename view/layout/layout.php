1c7e543bea5d1fec36b26a646790769c4d3695aa
namchee <cristophernamchee12@gmail.com>
2019-05-03 20:47:06 +0700

    Icon experiment

--- view/layout/layout.php
+++ view/layout/layout.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $title ?></title>
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
     <link rel="stylesheet" href="view/resources/styles/datatables.css">
     <link rel="stylesheet" href="view/resources/styles/datepicker.css">
     <link rel="stylesheet" href="view/resources/styles/slimselect.css">
